6d0b39cc0d34324e209533d338f43cce3fc54db9
WaioFaus <129754983+WaioFaus@users.noreply.github.com>
2023-04-24 15:49:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
-import sys
-import time
-from knowledge_base import KnowledgeBase
-from term import Term
-from Utils import process_string, unify, print_output
 import globals
-
-abstract_var = 1
-abstract_name = '_/*'
-not_have_anything_new = False
+from knowledge_base import KnowledgeBase
+from Utils import print_output
 
 globals.initialize()
     
-
 input_file = 'input.txt'
 output_file = 'output.txt'
-inference_choice = 0
-if len(sys.argv) == 2:
-    input_file = sys.argv[1]
-if len(sys.argv) == 3:
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
-if input_file =='':
-    input_file = input('Input file: ')
-if output_file == '':
-    output_file = input('Output file: ')
 
 knowledge_base = KnowledgeBase()
 knowledge_base.input_from_file(input_file)
 
-
-# while inference_choice <= 0 or inference_choice >=4:
-#     inference_choice = int(input('1. Forward chaining \t 2. Backward chaining \t 3. Resolution \nChoose inference: '))
 print("Backward chaining")
 f = open(output_file,'w')
 
